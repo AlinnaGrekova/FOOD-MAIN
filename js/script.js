db90329fab1596ce349552ac0a9b657e7db1a057
AlinnaGrekova <77895405+AlinnaGrekova@users.noreply.github.com>
2022-11-04 17:40:43 +0300

    timer and modal

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 window.addEventListener('DOMContentLoaded', ()=>{
-const tabs = document.querySelectorAll('.tabheader__item'),
-    tabsContent = document.querySelectorAll('.tabcontent'),
-    tabsParent = document.querySelector('.tabheader');
+    //tubs
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader');
 
-function hideTabContent(){
-    tabsContent.forEach(item=>{
-        item.style.display='none';
-    });
+    function hideTabContent(){
+        tabsContent.forEach(item=>{
+            item.style.display='none';
+        });
 
-    tabs.forEach(item=>{
-        item.classList.remove('tabheader__item_active');
-    });
-}
+        tabs.forEach(item=>{
+            item.classList.remove('tabheader__item_active');
+        });
+    }
 
-function showTabContent(i=0){
-    tabsContent[i].style.display='block';
-    tabs[i].classList.add('tabheader__item_active');
-}
+    function showTabContent(i=0){
+        tabsContent[i].style.display='block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
 
-hideTabContent();
-showTabContent();
+    hideTabContent();
+    showTabContent();
 
-tabsParent.addEventListener('click', (event)=>{
+    tabsParent.addEventListener('click', (event)=>{
     const target = event.target;
     if(target && target.classList.contains('tabheader__item')){
         tabs.forEach((item, i)=>{
@@ -29,8 +30,86 @@ tabsParent.addEventListener('click', (event)=>{
                 hideTabContent();
                 showTabContent(i);
             }
-        })
+        });
     }
-})
+    });
 
+    //timer
+
+    const timeEnd = '2022-11-20';
+    function getTimeRemaining(endtime){
+        const t=Date.parse(endtime)-Date.parse(new Date()),
+        days=Math.floor(t/(1000*60*60*24)),
+        hours=Math.floor(t/(1000*60*60*60)%24),
+        minutes=Math.floor((t/1000/60)%60),
+        seconds=Math.floor((t/1000)%60);
+        return{
+            'total':t,
+            'days':days,
+            'hours':hours,
+            'minutes':minutes,
+            'seconds':seconds
+        } ;
+    }
+    function setClock(selector, endtime){
+        const timer=document.querySelector(selector),
+        days= document.querySelector('#days'),
+        hours= document.querySelector('#hours'),
+        minutes= document.querySelector('#minutes'),
+        seconds= document.querySelector('#seconds'),
+        timeInterval=setInterval(updateClock,1000);
+        function updateClock(){
+            const t=getTimeRemaining(endtime);
+            days.innerHTML=t.days;
+            hours.innerHTML=t.hours;
+            minutes.innerHTML=t.minutes;
+            seconds.innerHTML=t.seconds;
+            if(t.total<=0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock('.timer', timeEnd);
+
+    //modal
+    function showhide(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow='';
+    }
+    const modalTriger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalClose = document.querySelector('[data-close]');
+
+    modalTriger.forEach(btn=>{
+        btn.addEventListener('click', ()=>{
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow='hidden';
+        });
+    });
+    modalClose.addEventListener('click', ()=>{
+    // modal.classList.add('hide');
+    // modal.classList.remove('show');
+    // document.body.style.overflow='';
+    showhide();
+    });
+
+    modal,addEventListener('click',(e)=>{
+    if(e.target===modal){
+        // modal.classList.add('hide');
+        // modal.classList.remove('show');
+        // document.body.style.overflow='';
+        showhide();
+    }
+    });
+
+    document.addEventListener('keydown', (e)=>{
+    if(e.code==='Escape'){
+        // modal.classList.add('hide');
+        // modal.classList.remove('show');
+        // document.body.style.overflow='';
+        showhide();
+    }
+    });
 });
